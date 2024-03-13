b9bd9e8d5ced2f5af3a99e2673b1c3d7a7db60c2
muza <muzallyefah@gmail.com>
2024-03-13 17:33:15 +0800

    feat: add logout button to home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import Cookies from 'universal-cookie';
 import { useNavigate } from 'react-router-dom';
+import reactLogo from '@/assets/react.svg';
+import logoutLogo from '@/assets/logout.svg';
 
 const Home = () => {
     const cookies = new Cookies();
@@ -17,8 +19,18 @@ const Home = () => {
     }, []);
 
     return (
-        <div className="flex bg-gray-900 h-screen">
-           <span className="text-gray-50 mx-auto my-auto">Home</span>
+        <div className="flex-col h-screen bg-gray-900 ">
+            <nav class="bg-white border-gray-200 dark:bg-gray-900">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <img src={reactLogo} alt="logo" className='w-6' />
+                    <button type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                        <img src={logoutLogo} alt="logo" className='w-5 h-5 text-white' />
+                    </button>
+                </div>
+            </nav>
+            <div className="flex h-full">
+                <span className="text-gray-50 mx-auto my-auto">Home</span>
+            </div>
         </div>
     );
 };
