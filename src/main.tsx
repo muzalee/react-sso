664fbe2ed0fa7b1f0eea0f35c8bfb10eae2e9fc4
muza <muzallyefah@gmail.com>
2024-03-13 15:56:22 +0800

    feat: setup tailwind and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={ import.meta.env.GOOGLE_CLIENT_ID as string }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <span className="text-3xl font-bold underline">
+            Hello world!
+        </span>
+    );
+};
+
+export default Login;
