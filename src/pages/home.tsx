3b4a3e4238460b65d6a533775eef306473033d21
Muzallyefah Fadli <85381407+muzalee@users.noreply.github.com>
2024-03-13 20:24:58 +0800

    feature: google sso (#1)
    * feat: install  react oauth and router
    
    * feat: setup tailwind and router
    
    * feat: add sign in with google ui
    
    * feat: implement google sso
    
    * feat: check for token
    
    * feat: add logout button to home
    
    * feat: logout

---
+++ src/pages/home.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import Cookies from 'universal-cookie';
+import { useNavigate } from 'react-router-dom';
+import reactLogo from '@/assets/react.svg';
+import logoutLogo from '@/assets/logout.svg';
+import { googleLogout  } from '@react-oauth/google';
+
+const Home = () => {
+    const cookies = new Cookies();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        document.title = "React SSO - Home";
+
+        const token = cookies.get('token') as string;
+        if (!token) {
+            navigate('/');
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const logout = () => {
+        googleLogout();
+        cookies.remove('token');
+        navigate('/');
+    }
+
+    return (
+        <div className="flex flex-col h-screen bg-gray-900 ">
+            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <img src={reactLogo} alt="logo" className='w-6' />
+                    <button type="button" onClick={logout} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                        <img src={logoutLogo} alt="logo" className='w-5 h-5 text-white' />
+                    </button>
+                </div>
+            </nav>
+            <div className="flex items-center justify-center grow">
+                <span className="text-gray-50">Home</span>
+            </div>
+        </div>
+    );    
+};
+
+export default Home;
---
+++ src/pages/login.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react';
+import reactLogo from '@/assets/react.svg';
+import googleLogo from '@/assets/google.svg';
+import { useGoogleLogin  } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+
+const Login = () => {
+    const navigate = useNavigate();
+    const cookies = new Cookies();
+
+    useEffect(() => {
+        document.title = "React SSO - Login";
+
+        const token = cookies.get('token') as string;
+        if (token) {
+            navigate('/home');
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+     }, []);
+
+    const login = useGoogleLogin({
+        onSuccess: res => {
+            cookies.set('token', res.access_token, {
+                maxAge: res.expires_in,
+            });
+            navigate('/home');
+        },
+    });
+
+    return (
+        <div className="bg-gray-900">
+            <div className="flex justify-center h-screen">
+                <div className="hidden bg-cover lg:block lg:w-2/3 login-bg">
+                    <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+                        <div>
+                            <h2 className="text-2xl font-bold text-white sm:text-3xl">React SSO</h2>
+
+                            <p className="max-w-xl mt-3 text-gray-300">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
+                                autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus
+                                molestiae
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+                    <div className="flex-1">
+                        <div className="text-center">
+                            <div className="flex justify-center mx-auto">
+                                <img className="w-auto h-7 sm:h-8" src={ reactLogo } alt="" />
+                            </div>
+
+                            <p className="mt-3 text-gray-300">Sign in to access your account</p>
+                        </div>
+
+                        <div className="mt-8">
+                            <button onClick={login} className="flex w-full items-center justify-center mt-4 transition-colors duration-300 transform border rounded-lg border-gray-700 text-gray-200 hover:bg-gray-600">
+                                <div className="px-4 py-2">
+                                    <img className="w-6 h-6" src={ googleLogo } alt="" />
+                                </div>
+
+                                <span className="w-5/6 px-4 py-3 font-bold text-center">Sign in with Google</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
